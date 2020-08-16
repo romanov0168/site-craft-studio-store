5251b6301725c090b9f7478ce388c8b1a2302746
Anton Romanov <46568691+romanov0168@users.noreply.github.com>
2020-08-16 14:25:17 +0300

    Add files via upload

---
+++ StoreWebApp/ViewModels/IndexViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using StoreWebApp.Models;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace StoreWebApp.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+        public SelectList SelectCategories { get; set; }
+
+        //public static implicit operator IndexViewModel(IndexViewModel v)
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
---
+++ StoreWebApp/ViewModels/ProductViewModel.cs
@@ -0,0 +1,14 @@
+﻿using StoreWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreWebApp.ViewModels
+{
+    public class ProductViewModel
+    {
+        public Product Product { get; set; }
+        public List<Product> RecommendedProducts { get; set; }
+    }
+}
